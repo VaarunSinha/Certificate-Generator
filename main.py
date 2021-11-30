8315e49bc909e95e3aefd6b18ecfff33e612db4c
Dev2212 <buddietube123@gmail.com>
2021-11-30 12:54:14 +0530

    updated positioning

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 from PIL import Image, ImageDraw, ImageFont
 
-font = ImageFont.truetype("WorkSans-Bold.ttf", 48)
+font = ImageFont.truetype("WorkSans-Bold.ttf", 60)
 
 name = input("Name of participant: \n")
 
 img = Image.open('cert-template.jpg')
 draw = ImageDraw.Draw(img)
-draw.text((340,370), name, (255,255,255), font)
+draw.text((340,325), name, (255,0,0), font)
 rgb_im = img.convert('RGB')
 rgb_im.save(f'certs/{name}.jpg')
