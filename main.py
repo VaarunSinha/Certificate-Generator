a1f016fb0ded097add310a26f5d8fe406c08cccb
Dev2212 <buddietube123@gmail.com>
2021-12-02 20:17:15 +0530

    Made inputs title cased added new file name rules

--- main.py
+++ main.py
@@ -10,27 +10,30 @@ lastname = input("Last Name: \n")
 
 img = Image.open('cert-template.jpg')
 draw = ImageDraw.Draw(img)
-name_middle = "No"
+name_middle = False
 middle_last = False
 last_middle = False
+
+name , lastname , middlename = name.title() , lastname.title() , middlename.title()
 
 if len(name + middlename + lastname) <= 13:
     draw.text((340,325), name + " "+ middlename + " "+ lastname, (255,0,0), font)
 elif len(name + middlename + lastname) > 13:
     if len(name + middlename) <= 13:
         draw.text((340,325), name + " " + middlename, (255,0,0), font)
-        name_middle = "Yes"
+        draw.text((340,400), lastname, (255,0,0), font)
+        name_middle = True
     else:
         draw.text((340,325),name, (255,0,0), font)
 
 print(name_middle)
-if name_middle == "No" and len(middlename + lastname) <= 13:
+if name_middle == False and len(middlename + lastname) <= 13:
     draw.text((340,400),middlename + " " + lastname, (255,0,0), font)
     last_middle = True
-elif last_middle==False and name_middle=="No":
+elif last_middle==False and name_middle==False:
     middlename = middlename[0].upper()
     draw.text((340,400),middlename + " " + lastname, (255,0,0), font)
     
 
 rgb_im = img.convert('RGB')
-rgb_im.save(f'certs/{name}.jpg')
+rgb_im.save(f'certs/{name+"_"+middlename+"_"+lastname}.jpg'.replace(" ", "").lower())
